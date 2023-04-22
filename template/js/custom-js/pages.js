5ce1f14964f183a0d911b0da51945040b7bb8a9d
joiasmilan <106594214+joiasmilan@users.noreply.github.com>
2023-04-22 10:33:50 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -99,15 +99,3 @@ else {
   console.log("Your Browser does not support speech Recognition");
   //info.textContent = "Your Browser does not support Speech Recognition";
 }
-
-
-// Add your custom JavaScript for storefront pages here.
-$('.header__search-input').keyup(function(){
-    $('body .search__input').val($(this).val()).[0].dispatchEvent(new Event('input'));
-});
-
-$('body').click(function(e){
-    if($(e.target).closest('.header__search').length == 0){
-        $('#instant-search .search__status .close').click();
-    }
-});
